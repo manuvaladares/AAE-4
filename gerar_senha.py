4b0b4cd832d11da2338fbf1cbb8d19db23924f22
manuvaladares <manuella.valadares@hotmail.com>
2025-06-24 20:59:39 -0300

    implementação final

--- gerar_senha.py
+++ gerar_senha.py
@@ -2,11 +2,12 @@ from random import choice
 import string
 
 class GeradorSenha:
-    def __init__(self, tamanho=8, minuscula=False, maiuscula=False, numero=False):
+    def __init__(self, tamanho=8, minuscula=False, maiuscula=False, numero=False, simbolo=False):
         self.tamanho = tamanho
         self.minuscula = minuscula
         self.maiuscula = maiuscula
         self.numero = numero
+        self.simbolo = simbolo
 
     def gerar(self):
         senha = ""
@@ -15,5 +16,26 @@ class GeradorSenha:
         if self.minuscula:
             caracteres += string.ascii_lowercase
             senha += choice(string.ascii_lowercase)
-
+            
+        if self.maiuscula:
+            caracteres += string.ascii_uppercase
+            senha += choice(string.ascii_uppercase)
+            
+        if self.numero:
+            caracteres += string.digits
+            senha += choice(string.digits)
         
+        if self.simbolo:
+            caracteres += "!@#$%&*"
+            senha += choice("!@#$%&*")
+        
+        if not caracteres:
+            raise ("Você deve selecionar pelo menos um critério para gerar uma senha")
+
+        while len(senha) < self.tamanho:
+            senha += choice(caracteres)
+            
+        return senha
+
+gerador = GeradorSenha(tamanho=10, minuscula=False, maiuscula=False, numero=False, simbolo=False)
+print(gerador.gerar())
--- test_gerar_senha.py
+++ test_gerar_senha.py
@@ -6,12 +6,23 @@ class TestGeradorSenha:
         senha = gerador.gerar()
         assert any(c.islower() for c in senha)
     
-    # def test_maiuscula(self):
-    #     gerador = GeradorSenha(maiuscula=True)
-    #     senha = gerador.gerar()
-    #     assert any(c.isupper() for c in senha)
+    def test_maiuscula(self):
+        gerador = GeradorSenha(maiuscula=True)
+        senha = gerador.gerar()
+        assert any(c.isupper() for c in senha)
     
-    # def test_numero(self):
-    #     gerador = GeradorSenha(numero=True)
-    #     senha = gerador.gerar()
-    #     assert any(c.isdigit() for c in senha)
+    def test_numero(self):
+        gerador = GeradorSenha(numero=True)
+        senha = gerador.gerar()
+        assert any(c.isdigit() for c in senha)
+        
+    def test_simbolo(self):
+        gerador = GeradorSenha(tamanho=8, simbolo=True)
+        senha = gerador.gerar()
+        assert any(c in "!@#$%&*" for c in senha)
+    
+    def test_tamanho_da_senha_gerada(self):
+        tamanho_esperado = 12
+        gerador = GeradorSenha(tamanho=tamanho_esperado, minuscula=True)
+        senha = gerador.gerar()
+        assert len(senha) == tamanho_esperado
